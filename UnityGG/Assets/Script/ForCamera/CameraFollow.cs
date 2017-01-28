066bf7b749f04d0e06562af4572fdf8821d8e705
ManinderjitS <sukhisembhy@hotamil.com>
2017-01-28 13:05:36 -0800

    Adding the latest working project

---
+++ UnityGG/Assets/Script/ForCamera/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Vector2 velocity;
+
+	public float smoothLineX;
+	public float smoothLineY;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothLineX);
+		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothLineY);
+
+		transform.position = new Vector3 (posX, posY, transform.position.z);
+	}
+}
